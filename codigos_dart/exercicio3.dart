b1291777be0b5a6b5d705ad47ba1a2ffe75324a5
Pedro-LucasCR <pedrolucasscr05@gmail.com>
2026-03-26 10:26:28 -0300

    cuidapet

--- codigos_dart/exercicio3.dart
+++ codigos_dart/exercicio3.dart
@@ -2,7 +2,89 @@ import "dart:io";
 import "dart:math";
 
 void main(){
-    print("Digite um número:");
+   /* print("Digite um número:");
     String num = stdin.readLineSync()!;
-    print("O numero digitado foi: $num a média arítmetica é: ${int.parse(num) / 2} }");
+    print("O numero digitado foi: $num a média arítmetica é: ${int.parse(num) / 2} }"); */
+
+    /*
+    int idade;
+    print("Digite a sua idade:");
+    idade = int.parse(stdin.readLineSync()!);
+
+    if(idade >= 18){
+        print("Você é maior de idade");
+    } else {
+        print("Você é menor de idade");
+    }
+
+    //Operador Ternário
+    print(idade >= 18 ? "Você é maior de idade" : "Você é menor de idade");
+
+    int valor;
+    print("Digite um valor:");
+    valor = int.parse(stdin.readLineSync()!);
+    printf("O valor é positivo.");
+     if(valor < 0){
+        printf("O valor é negativo.");
+     } else if(valor == 0){
+        printf("O valor é zero.");
+     }
+
+     int diaSemana;
+        print("Digite um número de 1 a 7 para o dia da semana:");
+        diaSemana = int.parse(stdin.readLineSync()!);
+        switch(diaSemana){
+            case 1:
+                print("Domingo");
+                break;
+            case 2:
+                print("Segunda-feira");
+                break;
+            case 3:
+                print("Terça-feira");
+                break;
+            case 4:
+                print("Quarta-feira");
+                break;
+            case 5:
+                print("Quinta-feira");
+                break;
+            case 6:
+                print("Sexta-feira");
+                break;
+            case 7:
+                print("Sábado");
+                break;
+            default:
+                print("Número inválido, digite um número de 1 a 7.");
+        }
+        */
+
+        //Comandos de repetição
+        for(int i = 1; i <= 10; i++){
+            print(i);
+        }
+
+        int aux = 10;
+        while(aux > 0){
+            print(aux);
+            aux--;
+        }
+
+        int aux2 = 10;
+        do{
+            print(aux2);
+            aux2--;
+        } while(aux2 > 0);
+
+        int num, soma;
+        soma = 0;
+        print("Digite um número:");
+        num = int.parse(stdin.readLineSync()!);
+        while(num != 0){
+            soma = soma + num;
+            print("Digite um valor:");
+            num = int.parse(stdin.readLineSync()!);
+        }
+        print("A soma dos valores digitados é: $soma");
 }
--- cuidapet/Cuidapet.dart
+++ cuidapet/Cuidapet.dart
@@ -16,4 +16,42 @@ void main(){
     nome = stdin.readLineSync()!;
     print("Prezado(a), $nome! Seja bem-vindo(a) à nossa loja Cuidapet!\n");
     print("Oferecemos em nossa loja produtos e serviços para seu PET. Para venda de produtos, procure o colaborador Junior e, para serviços como banho ou tosa, procure o colaborador Neto. Obrigado e esperamos que tenha uma ótima experiência em nossa loja!");
+
+    int opcao;
+    print("Digite o número da opção desejada: \n");
+    print("1 - Ver ofertas de Produtos");
+    print("2 - Ver ofertas de Serviços");
+    print("3 - Ver ofertas de Roupas");
+    print("4 - Ver novos serviços");
+    print("5 - Ver promoção 1");
+    print("6 - Ver promoção 2");
+
+    opcao = int.parse(stdin.readLineSync()!);
+
+    switch(opcao){
+        case 1:
+            print("Ração Royal Canin Indor 7,5kg com o valor promocional de R\$ 280,00");
+            break;
+        case 2:
+            print("Banho e tosa na promoção pelo preço do banho R\$ 54,00");
+            break;
+        case 3:
+            print("Ver ofertas de roupas");
+            print("Roupas em ofertas - Capa de chuva R\$ 59,99");
+             break;
+        case 4:
+            print("Ver novos serviços");
+            print("Novos serviços oferecidos: Hidratação de pelo R\$ 39,99 | Tosa higiênica R\$ 10,99 | Tingimento dos pelo R\$ 55,99");
+             break;
+        case 5:
+            print("Promoção 1: 10% de desconto");
+            print("Compre um saco de 15kg de ração (R\$ 345,99) e ganhe 10% de desconto no banho");
+            break;
+        case 6:
+            print("Promoção 2: 20% de desconto");
+            print("Compre um banho com tosa higiênica e hidratação e ganhe 20% de desconto no valor total");
+            break;
+                default:
+            print("Opção inválida, por favor selecione uma opção válida.");
+    }
 }
