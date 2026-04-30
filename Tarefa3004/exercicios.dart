4240bac3f0c51f77bdb79dabd34df8a9a365d54c
Pedro-LucasCR <pedrolucasscr05@gmail.com>
2026-04-30 10:44:44 -0300

    a

---
+++ Tarefa3004/exercicios.dart
@@ -0,0 +1,100 @@
+import "dart:io";
+import "dart:math";
+
+void main() {
+  // List<int> lista = List.filled(5, 0);
+  // for (int i = 0; i < lista.length; i++) {
+  //   if (i % 2 == 0) {
+  //     lista[i] = 1;
+  //   } else {
+  //     lista[i] = 0;
+  //   }
+  // }
+  // print(lista);
+
+ 
+  // List<int> lista = List.filled(5, 0);
+  // int soma = 0;
+  // for (int i = 0; i < 5; i++) {
+  //   stdout.write("Digite o número $i: ");
+  //   int numero = int.parse(stdin.readLineSync() ?? "0");
+  //   lista[i] = numero;
+  //   soma += numero;
+  // }
+  // print("Soma: $soma");
+  // print("Números digitados:");
+  // for (int i = 0; i < 5; i++) {
+  //   print(lista[i]);
+  // }
+
+  
+  // List<int> lista = List.filled(10, 0);
+  // int count = 0;
+  // for (int i = 0; i < 10; i++) {
+  //   stdout.write("Digite o número $i: ");
+  //   int numero = int.parse(stdin.readLineSync() ?? "0");
+  //   lista[i] = numero;
+  //   if (numero > 100) {
+  //     count++;
+  //   }
+  // }
+  // print("Quantidade de números maiores que 100: $count");
+  // print("Números digitados:");
+  // for (int i = 0; i < 10; i++) {
+  //   print(lista[i]);
+  // }
+
+
+  // List<int> lista = List.filled(10, 0);
+  // int somaImpares = 0;
+  // for (int i = 0; i < 10; i++) {
+  //   stdout.write("Digite o número $i: ");
+  //   int numero = int.parse(stdin.readLineSync() ?? "0");
+  //   lista[i] = numero;
+  //   if (numero % 2 != 0) {
+  //     somaImpares += numero;
+  //   }
+  // }
+  // print("Soma dos números ímpares: $somaImpares");
+
+  // List<double> notas = List.filled(10, 0.0);
+  // double maiorNota = -1.0;
+
+  // for (int i = 0; i < 10; i++) {
+  //   stdout.write("Digite a nota do aluno ${i + 1}: ");
+  //   double nota = double.parse(stdin.readLineSync() ?? "0.0");
+  //   notas[i] = nota;
+  //   if (nota > maiorNota) {
+  //     maiorNota = nota;
+  //   }
+  // }
+
+  // print("A maior nota da sala é: $maiorNota");
+
+  
+
+  // List<int> quadrados = List.filled(10, 0);
+  // for (int i = 0; i < quadrados.length; i++) {
+  //   quadrados[i] = i * i;
+  // }
+  // print(quadrados);
+
+ 
+
+  // List<int> somaPosicoes = List.filled(10, 0);
+  // for (int i = 0; i < somaPosicoes.length; i++) {
+  //   somaPosicoes[i] = i + i;
+  // }
+  // print(somaPosicoes);
+
+
+  // List<int> numeros = List.filled(5, 0);
+  // for (int i = 0; i < numeros.length; i++) {
+  //   stdout.write("Digite o número ${i + 1}: ");
+  //   numeros[i] = int.parse(stdin.readLineSync() ?? "0");
+  // }
+  // print("Números na ordem inversa:");
+  // for (int i = numeros.length - 1; i >= 0; i--) {
+  //   print(numeros[i]);
+  // }
+}
